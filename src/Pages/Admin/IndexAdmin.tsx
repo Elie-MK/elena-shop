dae82e1ad6e2b58d983ceed33a5356d5b0896f19
Elie-MK <elie.mk25@gmail.com>
2024-06-16 00:02:22 +0100

    add admin page

--- src/Pages/Admin/IndexAdmin.tsx
+++ src/Pages/Admin/IndexAdmin.tsx
@@ -1,8 +1,35 @@
+import Input from "@/components/ui/input"
+import { redirect } from "react-router-dom";
 
 function IndexAdmin() {
   return (
-    <div>IndexAdmin</div>
+    <div className="flex justify-center mt-10">
+      <div>
+      <h1 className="text-xl font-bold uppercase">Bonjour Admin</h1>
+      <form>
+        <Input type="text" title=" Title" />
+        <Input type="number" title=" Price" />
+        <Input type="text" title=" Image" />
+        <button type="button" className="block mt-8 mb-8 hover:bg-blue-900 border hover:text-white transition-all duration-300 rounded-lg font-bold border-blue-900 py-3 px-3 w-80">Publie</button>
+      </form>
+      </div>
+    </div>
   )
 }
 
 export default IndexAdmin
+
+export function loader(){
+  const userJson = localStorage.getItem('user');
+  if(userJson){
+    const user = JSON.parse(userJson)
+    if(!user.isAdmin){
+      return redirect('/')
+    }else {
+      return IndexAdmin
+    }
+  }else {
+    return redirect('/')
+  }
+  
+} 
